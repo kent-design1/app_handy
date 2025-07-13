0c083e68bb7d803d041cae75d2587b6493ba9f02
Kenneth <israel.kenneth2011@gmail.com>
2025-07-13 03:11:27 +0200

    intro

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import "./global.css"
+import {FlatList, Image, Pressable, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {offers} from "@/constants";
+import {Fragment} from "react";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Start</Text>
-    </View>
-  );
+export default function App() {
+    return (
+        <SafeAreaView className="">
+            <FlatList data={offers} renderItem={({item, index}) =>{
+
+               return (
+                   <View>
+                       <Pressable className= "offer-card" style={{backgroundColor: item.color}}>
+                           {({pressed}) => (
+                            <Fragment>
+                                <View className= "h-full w-1/2">
+                                    <Image source={item.image} className= "size-full" resizeMode={"contain"} />
+                                </View>
+
+                                <View className={"offer-card__info"}>
+                                        <Text> {item.title}</Text>
+                                </View>
+                            </Fragment>
+                           )}
+
+                       </Pressable>
+                   </View>
+               )
+            }} />
+        </SafeAreaView>
+    );
 }
