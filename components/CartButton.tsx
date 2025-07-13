9c8751bfa72be3ccb9e2af98aec64a4ca6afe134
Kenneth <israel.kenneth2011@gmail.com>
2025-07-13 14:31:55 +0200

    skeleton

---
+++ components/CartButton.tsx
@@ -0,0 +1,24 @@
+import {View, Text, TouchableOpacity, Image} from 'react-native'
+import React from 'react'
+import {images} from "@/constants";
+
+const CartButton = () => {
+
+    const totalItems = 10;
+
+    return (
+        <View>
+            <TouchableOpacity className="cart-btn" onPress={() => {}}>
+                <Image source={images.bag} resizeMode="contain" className="size-5"/>
+                {
+                    totalItems > 0 && (
+                        <View className="cart-badge">
+                            <Text className="small-bold text-white">{totalItems}</Text>
+                        </View>
+                    )
+                }
+            </TouchableOpacity>
+        </View>
+    )
+}
+export default CartButton
